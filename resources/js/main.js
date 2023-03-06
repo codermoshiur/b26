ccff647b7af6862083cab4420f1da1001dcecddb
codermoshiur <codermoshiur@gmail.com>
2023-03-06 15:59:42 +0600

    first commit

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+// MIXITUP
+var mixer = mixitup('.work-grid');
+
+// MENU
+function openMenu(){
+    document.getElementById('hamburger-menu').style.width = "100%";
+}
+
+function closeMenu(){
+    document.getElementById('hamburger-menu').style.width = "0%";
+}
